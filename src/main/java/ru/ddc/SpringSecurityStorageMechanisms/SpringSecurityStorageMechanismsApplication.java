feabf0c5ffbb8919e055681f3c7d79ee3ae6a5c8
Sergey Kovalev <dipdeepcode@mail.ru>
2023-07-08 22:34:59 +0300

    Init commit

---
+++ src/main/java/ru/ddc/SpringSecurityStorageMechanisms/SpringSecurityStorageMechanismsApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.SpringSecurityStorageMechanisms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStorageMechanismsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityStorageMechanismsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ddc/SpringSecurityStorageMechanisms/SpringSecurityStorageMechanismsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.SpringSecurityStorageMechanisms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStorageMechanismsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
